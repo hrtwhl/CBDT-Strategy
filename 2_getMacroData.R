ac91e1ff9920792cafabc7754df11e1b4c7b7cf3
Wilhelm Hartung <willituhartung@gmail.com>
2025-11-15 00:33:02 +0100

    Reduced number of macro data

--- 2_getMacroData.R
+++ 2_getMacroData.R
@@ -17,44 +17,44 @@ source("APIKey.R")
 # =========================================================
 fred_series <- c(
   # 1) USD & FX
-  usd_broad_index   = "DTWEXBGS",
-  usd_em_index      = "DTWEXEMEGS",
-  usd_afe_index     = "DTWEXAFEGS",
+  #usd_broad_index   = "DTWEXBGS",
+  #usd_em_index      = "DTWEXEMEGS",
+  #usd_afe_index     = "DTWEXAFEGS",
 
   # 2) U.S. Rates & Curve
-  fed_funds_rate    = "FEDFUNDS",
+  #fed_funds_rate    = "FEDFUNDS",
   yield_2y          = "DGS2",
-  yield_10y         = "DGS10",
+  #yield_10y         = "DGS10",
   term_spread_10y2y = "T10Y2Y",
-  term_spread_10y3m = "T10Y3M",
+  #term_spread_10y3m = "T10Y3M",
   breakeven_infl_10y= "T10YIE",
-  infl_exp_5y5y     = "T5YIFR",
-  real_yield_10y    = "DFII10",
+  #infl_exp_5y5y     = "T5YIFR",
+  #real_yield_10y    = "DFII10",
 
   # 3) Risk Appetite / Credit
-  vix_index         = "VIXCLS",
+  #vix_index         = "VIXCLS",
   hy_spread         = "BAMLH0A0HYM2",
-  bbb_spread        = "BAMLC0A4CBBB",
-  stl_fsi           = "STLFSI4",
-  chicago_fci       = "NFCI",
+  #bbb_spread        = "BAMLC0A4CBBB",
+  #stl_fsi           = "STLFSI4",
+  #chicago_fci       = "NFCI",
 
   # 5) Global / China Growth
-  china_cli         = "CHNLOLITOAASTSAM",
-  g7_cli            = "G7LOLITOAASTSAM",
-  oecd_cli          = "OECDLOLITOTRGYSAM",
+  #china_cli         = "CHNLOLITOAASTSAM",
+  #g7_cli            = "G7LOLITOAASTSAM",
+  #oecd_cli          = "OECDLOLITOTRGYSAM",
 
   # 6) U.S. Real Activity & Inflation
   industrial_prod   = "INDPRO",
-  nonfarm_payrolls  = "PAYEMS",
+  #nonfarm_payrolls  = "PAYEMS",
   unemployment_rate = "UNRATE",
-  initial_claims    = "ICSA",
-  continuing_claims = "CCSA",
-  real_retail_sales = "RRSFS",
-  cpi_headline      = "CPIAUCSL",
-  cpi_core          = "CPILFESL",
-  ppi_all_commodities = "PPIACO",
-  pce_index         = "PCEPI",
-  pce_core          = "PCEPILFE"
+  #initial_claims    = "ICSA",
+  #continuing_claims = "CCSA",
+  #real_retail_sales = "RRSFS",
+  #cpi_headline      = "CPIAUCSL",
+  #cpi_core          = "CPILFESL",
+  #ppi_all_commodities = "PPIACO",
+  #pce_index         = "PCEPI",
+  #pce_core          = "PCEPILFE"
 )
 
 # Helper: download FRED as long table
